da1c134654173f438aea17665f628140d6af5275
Ashutosh45c <dhokle100@gmail.com>
2024-09-06 17:48:22 +0530

    security check

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     ropsten: {
       url: 'https://eth-sepolia.g.alchemy.com/v2/1aw-axlOl2no39ZxJIeeS7KgrIHV-ExI',
-      accounts: ['edb3191d8fc589b8c2dd1ed6ee03282bd8346bbbe6ded3e91c9f02f16bc3411e'],
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
